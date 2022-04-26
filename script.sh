3b83d02a5e0a6918c58bbfe423bb786630319d79
Lin <gabrielvdiniz@gmail.com>
2022-04-25 21:18:22 -0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,90 @@
+# == LPGAI INSTALLER == #
+#part1
+printf '\033c'
+echo "Bem vindo ao instalador Lin Pretty Gay Arch Installer (LPGAI)"
+sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
+pacman --noconfirm -Sy archlinux-keyring
+loadkeys us
+timedatectl set-ntp true
+lsblk
+echo "Insira o hd (/dev/sdX), (/dev/nvmeX) "
+read drive
+fdisk $drive
+echo "Coloque a particao do Linux "
+read partition
+mkfs.ext4 $partition
+read -p "Voce criou a particao efi [s/n]" answer
+if [[ $answer = s ]] ; then
+  echo "Enter EFI partition: "
+  read efipartition
+  mkfs.vfat -F 32 $efipartition
+fi
+mount $partition /mnt
+pacstrap /mnt base base-devel linux linux-firmware networkamanger refind
+genfstab -U /mnt >> /mnt/etc/fstab
+sed '1,/^#part2$/d' `basename $0` > /mnt/arch_install2.sh
+chmod +x /mnt/arch_install2.sh
+arch-chroot /mnt ./arch_install2.sh
+exit
+
+#part2
+printf '\033c'
+pacman -S --noconfirm sed
+sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
+ln -sf /usr/share/zoneinfo/Brazil/East /etc/localtime
+hwclock --systohc
+echo "pt_BR.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=pt_BR.UTF-8" > /etc/locale.conf
+echo "KEYMAP=us" > /etc/vconsole.conf
+echo "Hostname: "
+read hostname
+echo $hostname > /etc/hostname
+echo "127.0.0.1       localhost" >> /etc/hosts
+echo "::1             localhost" >> /etc/hosts
+echo "127.0.1.1       $hostname.localdomain $hostname" >> /etc/hosts
+mkinitcpio -P
+passwd
+echo "Enter EFI partition: "
+read efipartition
+mkdir /boot/efi
+mount $efipartition /boot/efi
+refind-install
+
+pacman -S --noconfirm sway alacritty rsync htop nautilus \
+    firefox discord code ntfs-3g arc-gtk-theme ffmpegm \
+    neofetch neovim mpv qbittorrent git zathura zathura-pdf-mupdf \
+    bluez bluez-utils xdg-desktop-portal xdg-desktop-portal-wlr \
+    pipewire pipewire-media-session pipewire-alsa pipewire-pulse \
+    fish dash
+
+
+systemctl enable NetworkManager.service
+rm /bin/sh
+ln -s dash /bin/sh
+echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+echo "Enter Username: "
+read username
+useradd -m -G wheel -s /bin/fish $username
+passwd $username
+echo "Pre-Installation Finish Reboot now"
+ai3_path=/home/$username/arch_install3.sh
+sed '1,/^#part3$/d' arch_install2.sh > $ai3_path
+chown $username:$username $ai3_path
+chmod +x $ai3_path
+su -c $ai3_path -s /bin/sh $username
+exit
+#part3
+printf '\033c'
+cd $HOME
+git clone --separate-git-dir=$HOME/.dotfiles https://github.com/fnxln/dotfiles.git tmpdotfiles
+rsync --recursive --verbose --exclude '.git' tmpdotfiles/ $HOME/
+rm -r tmpdotfiles
+
+# pikaur: AUR helper
+git clone https://aur.archlinux.org/pikaur.git
+cd pikaur
+makepkg -fsri
+cd
+pikaur -S emacs-gcc-wayland-devel-bin
+mkdir dl dox imp music pix pub code
